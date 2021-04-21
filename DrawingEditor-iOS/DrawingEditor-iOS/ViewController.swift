5f18a87151e408ab55cb56c592dc01f17196aa2a
Muhsin Etki <muhsin.etki@boun.edu.tr>
2021-04-22 01:50:58 +0300

    Initial setup

---
+++ DrawingEditor-iOS/DrawingEditor-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrawingEditor-iOS
+//
+//  Created by Muhsin Etki on 22.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
